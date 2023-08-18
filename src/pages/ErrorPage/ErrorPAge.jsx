9fd8fed6cbcb37a3ed641ca564c8647c6e7de89e
Wanderlee Max <wanderleedev@gmail.com>
2023-08-17 22:34:11 -0500

    Page 404 terminada

--- src/pages/ErrorPage/ErrorPAge.jsx
+++ src/pages/ErrorPage/ErrorPAge.jsx
@@ -1,5 +1,5 @@
 import notFound from '@assets/404.gif'
-import {useRouteError} from 'react-router-dom'
+import {useRouteError, Link} from 'react-router-dom'
 
 const ErrorPage = () => {
   const error = useRouteError()
@@ -8,6 +8,9 @@ const ErrorPage = () => {
     <section className='min-h-screen grid place-content-center place-items-center bg-black'>
       <img src={notFound} alt="" />
       <h1>{`NOT FOUND: ${error.data}`}</h1>
+      <Link className='my-4 hover:underline bg-slate-300/80 py-2 px-4 rounded-md text-slate-900 font-semibold outline-offset-2 ' to='/'>
+        {`Back to Home`}
+      </Link>
     </section>
   )
 }
